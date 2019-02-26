e74dab4f495953e910c56b0efab102e32ea9c5aa
Mike Eling <mike.eling97@gmail.com>
2019-02-26 23:17:29 +0100

    ♻ Refactor Modal styles

---
+++ src/components/Modal/styles.js
@@ -0,0 +1,37 @@
+export default {
+  modal: {
+    position: "fixed",
+    zIndex: "1",
+    paddingTop: "100px",
+    left: "0",
+    top: "0",
+    width: "100%",
+    height: "100%",
+    overflow: "auto",
+    backgroundColor: "rgb(0,0,0)",
+    backgroundColor: "rgba(0,0,0,0.4)",
+  },
+  modalContent: {
+    backgroundColor: "#fefefe",
+    margin: "auto",
+    padding: "20px",
+    border: "1px solid #888",
+    width: "80 %",
+  },
+  title: {
+    textAlign: "center",
+    textTransform: "uppercase",
+    color: "#4CAF50",
+  },
+  content: {
+    textIndent: "50px",
+    textAlign: "justify",
+    letterSpacing: "3px",
+  },
+  actions: {
+    background: "#F9FAFB",
+    padding: "1rem 1rem",
+    borderTop: "1px solid rgba(34, 36, 38, 0.15)",
+    textAlign: "right",
+  },
+};
