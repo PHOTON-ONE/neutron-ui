d9b966a49ff527a73e04f1d1793fa122006a6cd9
Zigfrid <adam.stajszczak@gmail.com>
2019-03-03 14:19:16 +0100

    change css  of Button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,11 +4,24 @@ import withStyles from "react-jss";
 
 const styles = {
   root: {
-    border: "10px solid #99CC66",
-    display: "inline-block",
+    // border: "10px solid #99CC66",
+     display: "inline-block",
+    //  border: "10px dashed #BD0000",
+    margin: "5px",
+    // border: "0",
+    // position: "fixed",
+    top: "50%",
+    left: "50%",
+    // transform: "translate(-50%, -50%)",
+    background: "lightblue",
+    color: "white",
+    padding: "10px 20px",
+    borderRadius: "10px",
+    // fontSize: "21px",
+    
     cursor: "pointer",
     "&:active": {
-      border: "10px dashed #BD0000",
+      // border: "10px dashed #BD0000",
     },
   },
 };
